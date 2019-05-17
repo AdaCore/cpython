42f64ae25a45f21e3818ff5130c0b03866205cf6
Olivier Ramonat <ramonat@adacore.com>
2019-05-17 12:53:07 +0200

    Fix compilation issue of GetThreadTimeWithInfo on Solaris 9
    Use patch from https://bugs.python.org/issue35455
    to avoid compilation issue caused by missing CLOCK_THREAD_CPUTIME_ID
    
    TN: S319-010
    (cherry picked from commit b19bde45a5184ba5d28ad9534e18b8dfc5baa6f2)

--- Modules/timemodule.c
+++ Modules/timemodule.c
@@ -1344,6 +1344,26 @@ _PyTime_GetThreadTimeWithInfo(_PyTime_t *tp, _Py_clock_info_t *info)
     return 0;
 }
 
+/* Patch from https://bugs.python.org/file47984/thread_time.diff */
+#elif defined(__sun) && defined(__SVR4)
+#define HAVE_THREAD_TIME
+static int
+_PyTime_GetThreadTimeWithInfo(_PyTime_t *tp, _Py_clock_info_t *info)
+{
+    _PyTime_t t;
+
+    if (info) {
+        info->implementation = "gethrvtime()";
+        info->resolution = 1e-9;
+        info->monotonic = 1;
+        info->adjustable = 0;
+    }
+
+    t = _PyTime_FromNanoseconds(gethrvtime());
+    *tp = t;
+    return 0;
+}
+
 #elif defined(HAVE_CLOCK_GETTIME) && defined(CLOCK_PROCESS_CPUTIME_ID)
 #define HAVE_THREAD_TIME
 static int
